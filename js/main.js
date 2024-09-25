5bf43162e89b9d9738f85c4505e8cf66c313987a
valenscarponi <valentinoscarponi1@gmail.com>
2024-09-25 18:01:26 -0300

    Version Nueva

--- js/main.js
+++ js/main.js
@@ -1,22 +1,20 @@
 const nav = document.querySelector("#nav");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
-// const link = document.getElementsByClassName("navegacion--enlace");
-
+const body = document.body; // Seleccionamos el body
 
 abrir.addEventListener('click', () => {
     nav.classList.add("visible");
-})
-
-
-function link__presionar(){
-    return nav.classList.remove("visible");
-
-}
-
-
+    body.classList.add("menu-abierto"); 
+});
 
 cerrar.addEventListener('click', () => {
     nav.classList.remove("visible");
-})
+    body.classList.remove("menu-abierto"); 
+});
 
+function link__presionar() {
+    nav.classList.remove("visible");
+    body.classList.remove("menu-abierto"); 
+    return true;
+}
